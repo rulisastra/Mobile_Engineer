9fac219a280c3e4c112fe1d65dc34b7c45a4d489
Ruli Sastra <rulisastra@gmail.com>
2020-02-13 09:51:05 +0700

    Update json.js

--- Soal 2/json.js
+++ Soal 2/json.js
@@ -1,7 +1,11 @@
-var obj = {
-    ProductName : "Excelso" ,
-    Quantity : "1"
+var makanan = {
+    excelso : {ProductName : "Excelso", Quantity : "1"};
+    breadlife : {ProductName: "Breadlife", Quantity : "1"};
+}
+
+var ponsel = {
+    axis : {ProductName : "Axis, Quantity : "1"};
 };
 
-var stringObj = JSON.stringify(obj);
+var stringObj = JSON.stringify(makanan);
 console.log(stringObj);
